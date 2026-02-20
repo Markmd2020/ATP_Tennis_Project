3bd5bba4e4b3d6e94723ea5865fffde82641237c
Mark Montanana <markm8681@hotmail.com>
2026-02-20 20:58:40 +0000

    Add outlier detection example and enforce integrity constraints

---
+++ ATP2025_Outlier_Detection.R
@@ -0,0 +1,118 @@
+#############ATP 2025:Outliers detection######
+
+#Load libraries
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(reshape2)
+library(lubridate)
+library(GGally)
+library(scales)
+library(naniar)
+library(MASS)
+library(leaps)
+library(purrr)
+
+#Set seed for reproducibility
+set.seed(135)
+
+#Read data
+atp_2025 <- read.csv("C:/Applied_Stats_Project/Data/2025.csv")
+
+#Initial Summary
+str(atp_2025)
+
+#Descriptive statistics
+summary(atp_2025)
+
+#Prepare the data
+winners_df <-atp_2025 %>%
+  dplyr::select(surface,tourney_date,tourney_name, starts_with("w")) %>%
+  dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("w"))
+
+match_stats <- data.frame(rbind(losers_df,winners_df))
+head(match_stats)
+match_stats$win_game <- as.factor(match_stats$win_game)
+
+#Create win column
+winners_df$win_game <- "Yes"
+
+losers_df <- atp_2025 %>%
+  dplyr::select(surface,tourney_date,tourney_name, starts_with("l")) %>%
+  dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("l"))
+
+losers_df$win_game <- "No"
+
+#Quality checks before stacking both datasets
+all(dim(winners_df) ==dim(losers_df))
+all(names(winners_df)==names(losers_df))
+
+match_stats <- data.frame(rbind(losers_df,winners_df))
+head(match_stats)
+match_stats$win_game <- as.factor(match_stats$win_game)
+
+#Exclude missing values
+match_stats1 <- na.omit(match_stats)
+
+#Split into categorical and numerical variables
+atp_2025_cat_vars <- match_stats1 %>% dplyr::select(where(~ is.character(.x) || is.factor(.x)))
+atp_2025_num_vars   <- match_stats1 %>% dplyr::select(where(is.numeric))
+
+#Explore variable names
+colnames(atp_2025_cat_vars)
+colnames(atp_2025_num_vars)
+
+#Retrieve Hampel Filter Outlier function
+source("C:/Users/MarkM/OneDrive/Documents/ATP_Tennis_Project/Hampel_Outlier_Detection.R")
+
+#Explore age and height
+summary(atp_2025_num_vars$age)
+summary(atp_2025_num_vars$ht)
+
+#The method will be applied for numerical variables and will be focused on match stats
+match_metrics <-setdiff(colnames(atp_2025_num_vars),
+                        c("tourney_date","seed","rank","rank_points","age","ht"))
+
+#Exclude missing from outliers calculation
+match_stats2 <- atp_2025_num_vars[,match_metrics]
+
+#Apply Hampel Outlier function
+colSums(map_df(match_stats2,hampel_outlier_detection))/nrow(match_stats2)
+
+#Investigate if ranking and match outcome is related to outliers
+outlier_df <-data.frame(cbind(match_stats1[,c("name","win_game","rank")],
+                 map_df(match_stats2,hampel_outlier_detection)))
+head(outlier_df)
+
+outlier_summary_df <- outlier_df%>%
+      group_by(win_game)%>%
+      dplyr::summarise(across(all_of(match_metrics),~mean(.x,na.rm = TRUE),
+                              .names="outlier_pct_{.col}")) %>%
+  pivot_longer(
+    cols = starts_with("outlier_pct_"),
+    names_to = "metric",
+    values_to = "mean_value"
+  )
+
+#Data Visualisation
+ggplot(outlier_summary_df, aes(x = metric, y = round(mean_value,2), fill = win_game)) +
+  geom_col(position = "dodge") +
+  labs(
+    title = "Mean Outlier Percentages by Match Metric",
+    x = "Metric",
+    y = "Outlier Percentage",
+    fill = "Win Game"
+  ) +
+  scale_y_continuous(
+    limits = c(0, 0.1),
+    labels = scales::percent
+  )
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+
+
+
--- ATP_2025_Feature_Engineering.R
+++ ATP_2025_Feature_Engineering.R
@@ -24,20 +24,51 @@ str(atp_2025)
 
 #Descriptive statistics
 summary(atp_2025)
+head(atp_2025)
+
+#Add return statistics and enforce data integrity constraints
+atp_2025_cleaned <- atp_2025%>%
+            filter((l_1stIn>=l_1stWon) & (w_svpt - w_1stIn -w_df >=0) & (l_svpt - l_1stIn -l_df >0)
+                   & (w_bpFaced>=w_bpSaved) & (l_bpFaced>=l_bpSaved)
+                   & (w_svpt - w_1stIn -w_df >= w_2ndWon) & (l_svpt - l_1stIn -l_df >l_2ndWon) ) %>% 
+            mutate(w_1stRetIn=l_1stIn,
+                   w_1stRetWon=l_1stIn-l_1stWon,
+                   w_2ndRetIn=l_svpt - l_1stIn -l_df,
+                   w_2ndRetWon=w_2ndRetIn-l_2ndWon,
+                   w_bpIn =l_bpFaced,
+                   w_bpConverted=l_bpFaced-l_bpSaved,
+                   l_1stRetIn=w_1stIn,
+                   l_1stRetWon=w_1stIn-w_1stWon,
+                   l_2ndRetIn=w_svpt - w_1stIn -w_df,
+                   l_2ndRetWon=l_2ndRetIn-w_2ndWon,
+                   l_bpIn =w_bpFaced,
+                   l_bpConverted=w_bpFaced-w_bpSaved)
+summary(atp_2025_cleaned)
+atp_2025_cleaned%>%filter(w_2ndRetWon<0)
+atp_2025%>%filter(w_1stRetWon==-3)
+atp_2025%>% filter(l_1stIn<l_1stWon) %>% tally()#Constraint 1
+atp_2025%>% filter(l_2ndRetIn <0) %>% tally()
+atp_2025%>% filter(w_svpt - w_1stIn -w_df <0) %>% tally()#Constraint 2
+atp_2025%>% filter(l_svpt - l_1stIn -l_df <0) %>% tally()#Constraint 3
+atp_2025%>% filter(w_bpFaced<w_bpSaved) %>% tally()#Constraint 4
+atp_2025%>% filter(l_bpFaced<l_bpSaved) %>% tally()#Constraint 5
+atp_2025%>% filter(w_svpt - w_1stIn -w_df < w_2ndWon) %>% tally()#Constraint 6
+atp_2025%>% filter(l_svpt - l_1stIn -l_df < l_2ndWon) %>% tally()#Constraint 7
+
 
 #Prepare the data
-winners_df <-atp_2025 %>%
+winners_df <-atp_2025_cleaned %>%
   dplyr::select(surface,tourney_date,tourney_name, starts_with("w")) %>%
   dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("w"))
 
 #Create win column
 winners_df$win_game <- "Yes"
 
-losers_df <- atp_2025 %>%
+losers_df <- atp_2025_cleaned %>%
   dplyr::select(surface,tourney_date,tourney_name, starts_with("l")) %>%
   dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("l"))
 
-losers_df$win_game <- "No"
+losers_df$win_game <- "No"  
 
 #Quality checks before stacking both datasets
 all(dim(winners_df) ==dim(losers_df))
@@ -46,13 +77,23 @@ all(names(winners_df)==names(losers_df))
 match_stats <- data.frame(rbind(losers_df,winners_df))
 head(match_stats)
 match_stats$win_game <- as.factor(match_stats$win_game)
+colnames(match_stats)
 
 match_stats1 <-match_stats %>%
   mutate(first_point_serve_win_pct=X1stWon/X1stIn,
-         second_point_serve_win_pct=X2ndWon/(svpt - X1stIn -df),#Double check this metric
-         aces_df_ratio=ace/df,
+         second_point_serve_win_pct=X2ndWon/(svpt - X1stIn -df),
+         first_ret_win_pct = X1stRetWon/X1stRetIn,
+         second_ret_win_pct = X2ndRetWon/X2ndRetIn,
+         bp_conversion_pct=bpConverted/bpIn,
+         aces_df_ratio=ace/pmax(df,1),
          bp_saved_pct=bpSaved/bpFaced)
 head(match_stats1)
+summary(match_stats1)
+
+match_stats1 %>%
+  filter(X2ndRetIn<0)
+
+match_stats1%>% filter(X1stRetWon==-3)
 
 fit <- glm(win_game~second_point_serve_win_pct,family="binomial",data=match_stats1)
 summary(fit)
