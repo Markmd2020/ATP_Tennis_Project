c825601a68f29ce7e9561eac8353ceaaa293072a
Mark Montanana <markm8681@hotmail.com>
2026-02-21 14:25:00 +0000

    Added derived metrics for return ability

--- Stepwise_Models_By_Surface.R
+++ Stepwise_Models_By_Surface.R
@@ -21,19 +21,38 @@ set.seed(135)
 #Read data
 atp_2025 <- read.csv("C:/Applied_Stats_Project/Data/2025.csv")
 
+#Add return statistics and enforce data integrity constraints
+atp_2025_cleaned <- atp_2025%>%
+  filter((l_1stIn>=l_1stWon) & (w_svpt - w_1stIn -w_df >=0) & (l_svpt - l_1stIn -l_df >0)
+         & (w_bpFaced>=w_bpSaved) & (l_bpFaced>=l_bpSaved)
+         & (w_svpt - w_1stIn -w_df >= w_2ndWon) & (l_svpt - l_1stIn -l_df >l_2ndWon) ) %>% 
+  mutate(w_1stRetIn=l_1stIn,
+         w_1stRetWon=l_1stIn-l_1stWon,
+         w_2ndRetIn=l_svpt - l_1stIn -l_df,
+         w_2ndRetWon=w_2ndRetIn-l_2ndWon,
+         w_bpIn =l_bpFaced,
+         w_bpConverted=l_bpFaced-l_bpSaved,
+         l_1stRetIn=w_1stIn,
+         l_1stRetWon=w_1stIn-w_1stWon,
+         l_2ndRetIn=w_svpt - w_1stIn -w_df,
+         l_2ndRetWon=l_2ndRetIn-w_2ndWon,
+         l_bpIn =w_bpFaced,
+         l_bpConverted=w_bpFaced-w_bpSaved)
+summary(atp_2025_cleaned)
+
 #Prepare the data
-winners_df <-atp_2025 %>%
+winners_df <-atp_2025_cleaned %>%
   dplyr::select(surface,tourney_date,tourney_name, starts_with("w")) %>%
   dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("w"))
 
 #Create win column
 winners_df$win_game <- "Yes"
 
-losers_df <- atp_2025 %>%
-  dplyr::select(surface,tourney_date,tourney_name,starts_with("l")) %>%
+losers_df <- atp_2025_cleaned %>%
+  dplyr::select(surface,tourney_date,tourney_name, starts_with("l")) %>%
   dplyr::rename_with(~ sub("^[^_]*_", "", .x), starts_with("l"))
 
-losers_df$win_game <- "No"
+losers_df$win_game <- "No"  
 
 #Quality checks before stacking both datasets
 all(dim(winners_df) ==dim(losers_df))
@@ -41,23 +60,28 @@ all(names(winners_df)==names(losers_df))
 
 match_stats <- data.frame(rbind(losers_df,winners_df))
 head(match_stats)
-str(match_stats)
-match_stats$win_game  <- as.factor(match_stats$win_game)
+match_stats$win_game <- as.factor(match_stats$win_game)
+colnames(match_stats)
 
 #Feature Engineering
-head(match_stats)
 match_stats1 <-match_stats %>%
+  na.omit() %>%
   mutate(first_point_serve_win_pct=X1stWon/X1stIn,
-         second_point_serve_win_pct=X2ndWon/pmax((svpt - X1stIn -df),1),#Double check this metric
+         second_point_serve_win_pct=X2ndWon/(svpt - X1stIn -df),
+         first_ret_win_pct = X1stRetWon/X1stRetIn,
+         second_ret_win_pct = X2ndRetWon/X2ndRetIn,
+         bp_conversion_pct=bpConverted/bpIn,
          aces_df_ratio=ace/pmax(df,1),
-         bp_saved_pct=bpSaved/pmax(bpFaced,1))
+         bp_saved_pct=bpSaved/bpFaced)
+head(match_stats1)
+summary(match_stats1)
 
-names(match_stats1)
 vars_to_remove <- c("tourney_date","id","seed","entry","name","hand","ioc",
                     "rank","rank_points","X1stIn","X1stWon","X2ndWon",
-                    "SvGms","svpt")
-#Service Games and Service Points have been removed to target data leakage potential
+                    "SvGms","svpt","X1stRetWon","X1stRetIn","X2ndRetIn",
+                    "X2ndRetWon","bpIn","bpConverted")
 
+#Service Games and Service Points have been removed to target data leakage potential
 match_stats2 <-  match_stats1[,setdiff(names(match_stats1),vars_to_remove)]
 str(match_stats2)
 
@@ -176,7 +200,7 @@ ggplot(data = melted_cor, aes(x=Var1, y=Var2, fill=value)) +
         axis.title.y = element_blank(),
         panel.background = element_blank())
 
-clay_vars_to_remove <- c("bpSaved") 
+clay_vars_to_remove <- c("bpSaved")   
 
 clay_court1 <- clay_court[,setdiff(c("win_game",names(clay_court_num)),clay_vars_to_remove)]
 head(clay_court1)
